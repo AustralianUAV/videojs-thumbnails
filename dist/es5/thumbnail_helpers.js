beace26c77b41e8ee9a805463d43df0a892ef1ef
Andrew Chapman <amchapman@gmail.com>
2020-05-09 19:57:18 +1000

    adding dist/es5 build files

---
+++ dist/es5/thumbnail_helpers.js
@@ -0,0 +1,336 @@
+'use strict';
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+
+var _typeof = typeof Symbol === "function" && typeof Symbol.iterator === "symbol" ? function (obj) { return typeof obj; } : function (obj) { return obj && typeof Symbol === "function" && obj.constructor === Symbol && obj !== Symbol.prototype ? "symbol" : typeof obj; };
+
+var _createClass = function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; }();
+
+var _global = require('global');
+
+function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
+
+var ThumbnailHelpers = function () {
+  function ThumbnailHelpers() {
+    _classCallCheck(this, ThumbnailHelpers);
+  }
+
+  _createClass(ThumbnailHelpers, null, [{
+    key: 'hidePlayerOnHoverTime',
+    value: function hidePlayerOnHoverTime(progressControl) {
+      var mouseTime = progressControl.el_.getElementsByClassName('vjs-mouse-display')[0];
+
+      mouseTime.style.display = 'none';
+    }
+  }, {
+    key: 'createThumbnails',
+    value: function createThumbnails() {
+      for (var _len = arguments.length, args = Array(_len), _key = 0; _key < _len; _key++) {
+        args[_key] = arguments[_key];
+      }
+
+      var thumbnailClip = args.shift() || {};
+
+      Object.keys(args).map(function (i) {
+        var singleThumbnail = args[i];
+
+        Object.keys(singleThumbnail).map(function (property) {
+          if (singleThumbnail.hasOwnProperty(property)) {
+            if (_typeof(singleThumbnail[property]) === 'object') {
+              thumbnailClip[property] = ThumbnailHelpers.createThumbnails(thumbnailClip[property], singleThumbnail[property]);
+            } else {
+              thumbnailClip[property] = singleThumbnail[property];
+            }
+          }
+          return thumbnailClip;
+        });
+        return thumbnailClip;
+      });
+      return thumbnailClip;
+    }
+  }, {
+    key: 'getComputedStyle',
+    value: function getComputedStyle(thumbnailContent, pseudo) {
+      return function (prop) {
+        if (_global.window.getComputedStyle) {
+          return _global.window.getComputedStyle(thumbnailContent, pseudo)[prop];
+        }
+        return thumbnailContent.currentStyle[prop];
+      };
+    }
+  }, {
+    key: 'getVisibleWidth',
+    value: function getVisibleWidth(thumbnailContent, width) {
+      if (width) {
+        return parseFloat(width);
+      }
+
+      var clip = ThumbnailHelpers.getComputedStyle(thumbnailContent)('clip');
+
+      if (clip !== 'auto' && clip !== 'inherit') {
+        clip = clip.split(/(?:\(|\))/)[1].split(/(?:,| )/);
+        if (clip.length === 4) {
+          return parseFloat(clip[1]) - parseFloat(clip[3]);
+        }
+      }
+      return 0;
+    }
+  }, {
+    key: 'getScrollOffset',
+    value: function getScrollOffset() {
+      if (_global.window.pageXOffset) {
+        return {
+          x: _global.window.pageXOffset,
+          y: _global.window.pageYOffset
+        };
+      }
+      return {
+        x: _global.document.documentElement.scrollLeft,
+        y: _global.document.documentElement.scrollTop
+      };
+    }
+  }, {
+    key: 'suportAndroidEvents',
+    value: function suportAndroidEvents(player) {
+      // Android doesn't support :active and :hover on non-anchor and non-button elements
+      // so, we need to fake the :active selector for thumbnails to show up.
+      var progressControl = player.controlBar.progressControl;
+
+      var addFakeActive = function addFakeActive() {
+        progressControl.addClass('fake-active');
+      };
+
+      var removeFakeActive = function removeFakeActive() {
+        progressControl.removeClass('fake-active');
+      };
+
+      progressControl.on('touchstart', addFakeActive);
+      progressControl.on('touchend', removeFakeActive);
+      progressControl.on('touchcancel', removeFakeActive);
+    }
+  }, {
+    key: 'createThumbnaislHolder',
+    value: function createThumbnaislHolder() {
+      var wrap = _global.document.createElement('div');
+
+      wrap.className = 'vjs-thumbnail-holder';
+      return wrap;
+    }
+  }, {
+    key: 'createThumbnailImg',
+    value: function createThumbnailImg(thumbnailClips) {
+      var thumbnailImg = _global.document.createElement('img');
+
+      thumbnailImg.src = thumbnailClips['0'].src;
+      thumbnailImg.className = 'vjs-thumbnail-img';
+      return thumbnailImg;
+    }
+  }, {
+    key: 'createThumbnailTime',
+    value: function createThumbnailTime() {
+      var time = _global.document.createElement('div');
+
+      time.className = 'vjs-thumbnail-time';
+      time.id = 'vjs-time';
+      return time;
+    }
+  }, {
+    key: 'createThumbnailArrowDown',
+    value: function createThumbnailArrowDown() {
+      var arrow = _global.document.createElement('div');
+
+      arrow.className = 'vjs-thumbnail-arrow';
+      arrow.id = 'vjs-arrow';
+      return arrow;
+    }
+  }, {
+    key: 'mergeThumbnailElements',
+    value: function mergeThumbnailElements(thumbnailsHolder, thumbnailImg, timelineTime, thumbnailArrowDown) {
+
+      thumbnailsHolder.appendChild(thumbnailImg);
+      thumbnailsHolder.appendChild(timelineTime);
+      thumbnailsHolder.appendChild(thumbnailArrowDown);
+      return thumbnailsHolder;
+    }
+  }, {
+    key: 'centerThumbnailOverCursor',
+    value: function centerThumbnailOverCursor(thumbnailImg) {
+      // center the thumbnail over the cursor if an offset wasn't provided
+      if (!thumbnailImg.style.left && !thumbnailImg.style.right) {
+        thumbnailImg.onload = function () {
+          var thumbnailWidth = { width: -(thumbnailImg.naturalWidth / 2) };
+
+          thumbnailImg.style.left = thumbnailWidth + 'px';
+        };
+      }
+    }
+  }, {
+    key: 'getVideoDuration',
+    value: function getVideoDuration(player) {
+      var duration = player.duration();
+
+      player.on('durationchange', function () {
+        duration = player.duration();
+      });
+      return duration;
+    }
+  }, {
+    key: 'addThumbnailToPlayer',
+    value: function addThumbnailToPlayer(progressControl, thumbnailsHolder) {
+      progressControl.el().appendChild(thumbnailsHolder);
+    }
+  }, {
+    key: 'findMouseLeftOffset',
+    value: function findMouseLeftOffset(pageMousePositionX, progressControl, pageXOffset, event) {
+      // find the page offset of the mouse
+      var leftOffset = pageMousePositionX || event.clientX + _global.document.body.scrollLeft + _global.document.documentElement.scrollLeft;
+
+      // subtract the page offset of the positioned offset parent
+      leftOffset -= progressControl.el().getBoundingClientRect().left + pageXOffset;
+      return leftOffset;
+    }
+  }, {
+    key: 'getMouseVideoTime',
+    value: function getMouseVideoTime(mouseLeftOffset, progressControl, duration) {
+      return Math.floor(mouseLeftOffset /*- progressControl.el().offsetLeft*/ / progressControl.width() * duration);
+    }
+  }, {
+    key: 'updateThumbnailTime',
+    value: function updateThumbnailTime(timelineTime, progressControl) {
+      timelineTime.innerHTML = progressControl.seekBar.mouseTimeDisplay.timeTooltip.el_.textContent;
+    }
+  }, {
+    key: 'getPageMousePositionX',
+    value: function getPageMousePositionX(event) {
+      var pageMouseOffsetX = event.pageX;
+
+      if (event.changedTouches) {
+        pageMouseOffsetX = event.changedTouches[0].pageX;
+      }
+      return pageMouseOffsetX;
+    }
+  }, {
+    key: 'keepThumbnailInsidePlayer',
+    value: function keepThumbnailInsidePlayer(thumbnailImg, activeThumbnail, thumbnailClips, mouseLeftOffset, progresBarRightOffset) {
+
+      var width = ThumbnailHelpers.getVisibleWidth(thumbnailImg, activeThumbnail.width || thumbnailClips[0].width);
+
+      var halfWidth = width / 2;
+
+      // make sure that the thumbnail doesn't fall off the right side of
+      // the left side of the player
+      if (mouseLeftOffset + halfWidth > progresBarRightOffset) {
+        mouseLeftOffset -= mouseLeftOffset + halfWidth - progresBarRightOffset;
+      } else if (mouseLeftOffset < halfWidth) {
+        mouseLeftOffset = halfWidth;
+      }
+      return mouseLeftOffset;
+    }
+  }, {
+    key: 'updateThumbnailLeftStyle',
+    value: function updateThumbnailLeftStyle(mouseLeftOffset, thumbnailsHolder) {
+      var leftValue = { mouseLeftOffset: mouseLeftOffset };
+
+      thumbnailsHolder.style.left = leftValue.mouseLeftOffset + 'px';
+    }
+  }, {
+    key: 'getActiveThumbnail',
+    value: function getActiveThumbnail(thumbnailClips, mouseTime) {
+      var activeClip = 0;
+
+      for (var clipNumber in thumbnailClips) {
+        if (mouseTime > clipNumber) {
+          activeClip = Math.max(activeClip, clipNumber);
+        }
+      }
+      return thumbnailClips[activeClip];
+    }
+  }, {
+    key: 'updateThumbnailSrc',
+    value: function updateThumbnailSrc(activeThumbnail, thumbnailImg) {
+      if (activeThumbnail.src && thumbnailImg.src !== activeThumbnail.src) {
+        thumbnailImg.src = activeThumbnail.src;
+      }
+    }
+  }, {
+    key: 'updateThumbnailStyle',
+    value: function updateThumbnailStyle(activeThumbnail, thumbnailImg) {
+      if (activeThumbnail.style && thumbnailImg.style !== activeThumbnail.style) {
+        ThumbnailHelpers.createThumbnails(thumbnailImg.style, activeThumbnail.style);
+      }
+    }
+  }, {
+    key: 'moveListener',
+    value: function moveListener(event, progressControl, thumbnailsHolder, thumbnailClips, timelineTime, thumbnailImg, player) {
+
+      var duration = ThumbnailHelpers.getVideoDuration(player);
+      var pageXOffset = ThumbnailHelpers.getScrollOffset().x;
+      var progresBarPosition = progressControl.children()[0].el().getBoundingClientRect();
+
+      var progresBarRightOffset = (progresBarPosition.width || progresBarPosition.right) + pageXOffset;
+
+      var pageMousePositionX = ThumbnailHelpers.getPageMousePositionX(event);
+
+      var mouseLeftOffset = ThumbnailHelpers.findMouseLeftOffset(pageMousePositionX, progressControl.children()[0], pageXOffset, event);
+
+      var mouseTime = ThumbnailHelpers.getMouseVideoTime(mouseLeftOffset, progressControl.children()[0], duration);
+
+      var activeThumbnail = ThumbnailHelpers.getActiveThumbnail(thumbnailClips, mouseTime);
+
+      ThumbnailHelpers.updateThumbnailTime(timelineTime, progressControl);
+
+      ThumbnailHelpers.updateThumbnailSrc(activeThumbnail, thumbnailImg);
+
+      ThumbnailHelpers.updateThumbnailStyle(activeThumbnail, thumbnailImg);
+
+      mouseLeftOffset = ThumbnailHelpers.keepThumbnailInsidePlayer(thumbnailImg, activeThumbnail, thumbnailClips, mouseLeftOffset, progresBarRightOffset);
+
+      ThumbnailHelpers.updateThumbnailLeftStyle(mouseLeftOffset, thumbnailsHolder);
+
+      //console.log("moveListener timelineTime: " + timelineTime + ", activeThumbnail: " + activeThumbnail);
+      //console.log("pageMousePositionX: " + pageMousePositionX + ", mouseLeftOffset: " + mouseLeftOffset);
+      //console.dir(activeThumbnail);
+    }
+  }, {
+    key: 'upadateOnHover',
+    value: function upadateOnHover(progressControl, thumbnailsHolder, thumbnailClips, timelineTime, thumbnailImg, player) {
+
+      // update the thumbnail while hovering
+      progressControl.on('mousemove', function (event) {
+        ThumbnailHelpers.moveListener(event, progressControl, thumbnailsHolder, thumbnailClips, timelineTime, thumbnailImg, player);
+      });
+      progressControl.on('touchmove', function (event) {
+        ThumbnailHelpers.moveListener(event, progressControl, thumbnailsHolder, thumbnailClips, timelineTime, thumbnailImg);
+      });
+    }
+  }, {
+    key: 'hideThumbnail',
+    value: function hideThumbnail(thumbnailsHolder) {
+      thumbnailsHolder.style.left = '-1000px';
+    }
+  }, {
+    key: 'upadateOnHoverOut',
+    value: function upadateOnHoverOut(progressControl, thumbnailsHolder, player) {
+
+      // move the placeholder out of the way when not hovering
+      progressControl.on('mouseout', function (event) {
+        ThumbnailHelpers.hideThumbnail(thumbnailsHolder);
+      });
+      progressControl.on('touchcancel', function (event) {
+        ThumbnailHelpers.hideThumbnail(thumbnailsHolder);
+      });
+      progressControl.on('touchend', function (event) {
+        ThumbnailHelpers.hideThumbnail(thumbnailsHolder);
+      });
+      player.on('userinactive', function (event) {
+        ThumbnailHelpers.hideThumbnail(thumbnailsHolder);
+      });
+    }
+  }]);
+
+  return ThumbnailHelpers;
+}();
+
+exports.default = ThumbnailHelpers;
